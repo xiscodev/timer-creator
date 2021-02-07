ae2d0f99987662937e05dec905498c7f76e1dff8
José Diaz <xoxefdp@gmail.com>
2021-02-07 12:51:58 +0100

    Added tests for Creators/interval module

--- src/Creators/interval.js
+++ src/Creators/interval.js
@@ -1,18 +1,92 @@
 import launchCallback from '../Helpers/callback'
-import { pushToStore } from '../Helpers/store'
+import { _clearStore, initializeStore, pushToStore, isStored, getStoredItem, removeFromStore } from '../Helpers/store'
 
-const intervalStore = new Map()
+/**
+ * @access private
+ * @description Store for intervals.
+ * @type {Map}
+ */
+const _store = initializeStore()
 
-const createInterval = (func, time, args) => {
-  const intervalRef = setInterval(() => {
-    launchCallback(func, args)
-  }, time)
-  pushToStore(intervalStore, intervalRef)
+/**
+ * @access private
+ * @function _resetStore
+ * @description Clears interval _store.
+ */
+const _resetStore = () => {
+  _clearStore(_store)
 }
 
-const existInterval = ''
+/**
+ * @access private
+ * @function _removeInterval
+ * @description Removes stored intervals with given name.
+ * @param {string} name
+ */
+const _removeInterval = (name) => {
+  removeFromStore(_store, name)
+}
+
+/**
+ * @access public
+ * @function createInterval
+ * @description Creates and _store interval object with given name,
+ * to execute callback function on the waitTime specified with given args.
+ * @param {string} name
+ * @param {Function} callback
+ * @param {TimeUnit} waitTime
+ * @param {string|Array|NULL} args
+ */
+const createInterval = (name, callback, waitTime, args) => {
+  if (!existInterval(name)) {
+    const reference = setInterval(() => {
+      launchCallback(callback, args)
+    }, waitTime)
+    pushToStore(_store, name, reference)
+  }
+}
+
+/**
+ * @access public
+ * @function existInterval
+ * @description Checks whether exist interval with given name.
+ * @param {string} name
+ * @returns {boolean}
+ */
+const existInterval = (name) => {
+  return isStored(_store, name)
+}
+
+/**
+ * @access public
+ * @function getInterval
+ * @description Retrieves interval value for given name.
+ * @param {string} name
+ * @returns {number}
+ */
+const getInterval = (name) => {
+  return getStoredItem(_store, name)
+}
+
+/**
+ * @access public
+ * @function destroyInterval
+ * @description Destroy interval with given name and removes it from _store.
+ * @param {string} name
+ */
+const destroyInterval = (name) => {
+  if (existInterval(name)) {
+    clearInterval(getInterval(name))
+    _removeInterval(name)
+  }
+}
 
 export {
+  _store,
+  _resetStore,
+  _removeInterval,
   createInterval,
-  existInterval
+  existInterval,
+  getInterval,
+  destroyInterval,
 }
--- src/Creators/interval.spec.js
+++ src/Creators/interval.spec.js
@@ -1,14 +1,41 @@
-import { createInterval, existInterval } from './interval'
+import { _store, _resetStore, createInterval, existInterval, getInterval, destroyInterval } from './interval'
 
+// ENVIRONMENT VARIABLES
 jest.useFakeTimers()
 
-const intervalTime = 2000
+const wait = 3000
 const args = ['many', 'arguments', 'here']
-// const intervalName = 'interval_name'
+const INTERVAL_NAME = 'INTERVAL_TEST_NAME'
+const OTHER_INTERVAL_NAME = 'OTHER_INTERVAL_TEST_NAME'
 
-const _reset = () => {
+const _resetEnv = () => {
+  _resetStore()
   jest.clearAllTimers()
 }
+
+const _dummySetKey = 'aKey'
+const _dummySetValue = 2
+
+
+// TESTS
+describe('_store', () => {
+  it('should exist', () => {
+    expect(_store).not.toBeUndefined()
+  })
+
+  it('should be empty on initial state', () => {
+    _resetEnv()
+    expect(_store.size).toBe(0)
+  })
+
+  it('should not be empty after store first key/value pair', () => {
+    _resetEnv()
+
+    _store.set(_dummySetKey, _dummySetValue)
+
+    expect(_store.size).not.toBe(0)
+  })
+})
 
 describe('createInterval', () => {
   it('should exist', () => {
@@ -20,40 +47,72 @@ describe('createInterval', () => {
   })
 
   it('should run a callback', () => {
-    _reset()
+    _resetEnv()
     const testFn = jest.fn()
-    createInterval(testFn)
+    createInterval(INTERVAL_NAME, testFn)
 
     expect(testFn).not.toBeCalled()
 
     jest.runOnlyPendingTimers()
-
     expect(testFn).toHaveBeenCalledTimes(1)
   })
 
   it('should run a callback with given time', () => {
-    _reset()
+    _resetEnv()
     const testFn = jest.fn()
-    createInterval(testFn, intervalTime)
+    createInterval(INTERVAL_NAME, testFn, wait)
 
     expect(testFn).not.toBeCalled()
 
-    jest.advanceTimersByTime(intervalTime)
-
+    jest.advanceTimersByTime(wait)
     expect(testFn).toHaveBeenCalledTimes(1)
   })
 
-  it('should run a callback with arguments in given time', () => {
-    _reset()
+  it('should run a callback twice when given time occurs twice', () => {
+    _resetEnv()
     const testFn = jest.fn()
-    createInterval(testFn, intervalTime, args)
+    createInterval(INTERVAL_NAME, testFn, wait)
 
     expect(testFn).not.toBeCalled()
 
-    jest.advanceTimersByTime(intervalTime)
+    jest.advanceTimersByTime(wait)
+    jest.advanceTimersByTime(wait)
+    expect(testFn).toHaveBeenCalledTimes(2)
+  })
 
+  it('should run a callback with arguments in given time', () => {
+    _resetEnv()
+    const testFn = jest.fn()
+    createInterval(INTERVAL_NAME, testFn, wait, args)
+
+    expect(testFn).not.toBeCalled()
+
+    jest.advanceTimersByTime(wait)
     expect(testFn).toHaveBeenCalledTimes(1)
     expect(testFn).toHaveBeenLastCalledWith(...args)
+  })
+
+  it('should admit a name', () => {
+    _resetEnv()
+    const testFn = jest.fn()
+    createInterval(INTERVAL_NAME, testFn, wait)
+
+    expect(testFn).not.toBeCalled()
+
+    jest.advanceTimersByTime(wait)
+    expect(testFn).toHaveBeenCalledTimes(1)
+  })
+
+  it('should not create one if already exist with given name', () => {
+    _resetEnv()
+    const testFn = jest.fn()
+    createInterval(INTERVAL_NAME, testFn, wait)
+    createInterval(INTERVAL_NAME, testFn, wait)
+
+    expect(testFn).not.toBeCalled()
+
+    jest.advanceTimersByTime(wait)
+    expect(testFn).toHaveBeenCalledTimes(1)
   })
 })
 
@@ -64,5 +123,100 @@ describe('existInterval', () => {
 
   it('should be a function', () => {
     expect(existInterval())
+  })
+
+  it('should be false if interval does not exist', () => {
+    _resetEnv()
+
+    expect(existInterval(INTERVAL_NAME)).toBeFalsy()
+  })
+
+  it('should be true if interval exist', () => {
+    _resetEnv()
+    const testFn = jest.fn()
+    createInterval(INTERVAL_NAME, testFn, wait)
+
+    expect(existInterval(INTERVAL_NAME)).toBeTruthy()
+  })
+
+  it('should be false if interval does not exist and not on initial state', () => {
+    _resetEnv()
+    const testFn = jest.fn()
+    createInterval(INTERVAL_NAME, testFn, wait)
+
+    expect(existInterval(OTHER_INTERVAL_NAME)).toBeFalsy()
+  })
+})
+
+describe('getInterval', () => {
+  it('should exist', () => {
+    expect(getInterval).not.toBeUndefined()
+  })
+
+  it('should be a function', () => {
+    expect(getInterval())
+  })
+
+  it('should be undefined if name does not exist', () => {
+    _resetEnv()
+    expect(getInterval(INTERVAL_NAME)).toBeUndefined()
+  })
+
+  it('should not to be undefined if name exist', () => {
+    _resetEnv()
+    const testFn = jest.fn()
+    createInterval(INTERVAL_NAME, testFn, wait)
+
+    expect(getInterval(INTERVAL_NAME)).not.toBeUndefined()
+  })
+
+  it('should be undefined if name does not exist and not initial state', () => {
+    _resetEnv()
+    const testFn = jest.fn()
+    createInterval(INTERVAL_NAME, testFn, wait)
+
+    expect(getInterval(OTHER_INTERVAL_NAME)).toBeUndefined()
+  })
+})
+
+describe('destroyInterval', () => {
+  it('should exist', () => {
+    expect(destroyInterval).not.toBeUndefined()
+  })
+
+  it('should be a function', () => {
+    expect(destroyInterval())
+  })
+
+  it('should destroy given name from store if exist', () => {
+    _resetEnv()
+    const testFn = jest.fn()
+    createInterval(INTERVAL_NAME, testFn, wait)
+
+    destroyInterval(INTERVAL_NAME)
+
+    expect(getInterval(INTERVAL_NAME)).toBeUndefined()
+  })
+
+  it('should not destroy given others name from store', () => {
+    _resetEnv()
+    const testFn = jest.fn()
+    createInterval(INTERVAL_NAME, testFn, wait)
+
+    destroyInterval(OTHER_INTERVAL_NAME)
+
+    expect(getInterval(INTERVAL_NAME)).not.toBeUndefined()
+  })
+
+  it('should not run callback if destroyed before wait time', () => {
+    _resetEnv()
+    const testFn = jest.fn()
+    createInterval(INTERVAL_NAME, testFn, wait)
+
+    destroyInterval(INTERVAL_NAME)
+
+    jest.advanceTimersByTime(wait)
+
+    expect(testFn).not.toBeCalled()
   })
 })
